c5851b2acf2a8e59f76550a57a71e10860ed7c48
Isira Adithya <rp.isira@gmail.com>
2025-03-20 20:03:13 +0530

    configured expresssession with prisma

--- server/backend/routes/api.js
+++ server/backend/routes/api.js
@@ -1,8 +1,34 @@
 import express from "express";
+import expressSession from "express-session";
+import { PrismaClient } from "@prisma/client";
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 const router = express.Router();
 
+// Session Handler
+router.use(
+    expressSession({
+        cookie: {
+         maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+         secure: process.env.NODE_ENV == "development" ? "false" : "true", // Set to true if using HTTPS
+         httpOnly: true,
+         sameSite: 'strict',
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new PrismaSessionStore(
+          new PrismaClient(),
+          {
+            checkPeriod: 60 * 1000,  //ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+          }
+        )
+      })
+);
+
 // routes
-import alertsRouter from "./api/alerts.js";
+import alertsRouter from "./api-routes/alerts.js";
 
 // mount routes
 router.use("/alerts", alertsRouter);
