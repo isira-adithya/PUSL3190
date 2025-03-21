b6d00c9fa9ba7ea9306abfccdf3b72d26b10e51d
Isira Adithya <rp.isira@gmail.com>
2025-03-21 16:26:30 +0530

    added frontend layout files

---
+++ server/frontend/src/layout/AppMenu.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'XSSpecter',
+        items: [
+            { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
+            { label: 'Alerts', icon: 'pi pi-fw pi-bolt', to: '/alerts' },
+            { label: 'Account', icon: 'pi pi-fw pi-user', to: '/account', class: 'rotated-icon' },
+            { label: 'Settings', icon: 'pi pi-fw pi-cog', to: '/settings' },
+            { label: 'Logout', icon: 'pi pi-fw pi-sign-out', to: '/logout' }
+        ]
+    },
+    {
+        label: 'Links',
+        items: [
+            {
+                label: 'Documentation',
+                icon: 'pi pi-fw pi-book',
+                to: 'https://github.com/isira-adithya/PUSL3190/blob/main/Documentation.md',
+                target: '_blank'
+            },
+            {
+                label: 'View Source',
+                icon: 'pi pi-fw pi-github',
+                url: 'https://github.com/isira-adithya/xsspecter',
+                target: '_blank'
+            }
+        ]
+    }
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i" :key="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator" :key="`sep-${i}`"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
