55f76ef8da5bb312a0e856192487b7d59681e619
Isira Adithya <rp.isira@gmail.com>
2025-03-21 22:14:17 +0530

    API Token authentication added to the auth middleware

--- server/backend/middlewares/auth.js
+++ server/backend/middlewares/auth.js
@@ -1,8 +1,37 @@
-function checkAuth(req, res, next) {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function checkAuth(req, res, next) {
 
   // Give access to /api/users/login
   if (req.path.toLowerCase() === '/users/login') {
     return next();
+  }
+
+  // Check if req.path starts with /api/cli/
+  if (req.path.toLowerCase().startsWith('/cli/')) {
+
+    // Check if Authorization header is present
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.split(' ')[1];
+
+      // Check if token is valid
+      const count = await prisma.user.count({
+        where: {
+          apiToken: token
+        }
+      });
+
+      if (count > 0) {
+        return next();
+      } else {
+        res.status(403).json({ message: 'Forbidden' });
+        return;
+      }
+    } else {
+      res.status(403).json({ message: 'Forbidden' });
+      return;
+    }
   }
 
   // check if logged in
