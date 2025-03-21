2c541bc9d004e823d14196d0621b3270e0d1daab
Isira Adithya <rp.isira@gmail.com>
2025-03-21 19:48:24 +0530

    fixed account lastlogin bug added /generate-token endpoint added user acc delete route

--- server/backend/routes/api-routes/users.js
+++ server/backend/routes/api-routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import {body, validationResult} from "express-validator";
+import crypto from "crypto";
 import * as argon2 from "argon2";
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -40,6 +41,7 @@ router.post(
             req.session.isLoggedIn = true;
             req.session.user = user;
             req.session.user.lastUpdatedDB = new Date();
+            req.session.user.lastLogin = new Date();
             
             // Remove password from user object
             delete req.session.user.password; 
@@ -71,7 +73,7 @@ router.get("/me", async (req, res) => {
         const lastUpdatedDB = new Date(req.session.user.lastUpdatedDB);
         const currentTime = new Date();
         const diffInMinutes = Math.floor((currentTime - lastUpdatedDB) / (1000 * 60));
-        if (diffInMinutes > 3) {
+        if (diffInMinutes >= 1) {
             // Fetch user data from the database
             const updatedUser = await prisma.user.findUnique({
                 where: {
@@ -80,9 +82,10 @@ router.get("/me", async (req, res) => {
             });
             req.session.user = updatedUser;
             req.session.user.lastUpdatedDB = new Date();
+            delete req.session.user.password;
         }
     }
-    res.status(200).json(user);
+    res.status(200).json(req.session.user);
 });
 
 // update account
@@ -146,6 +149,7 @@ router.put(
     }
 );
 
+// change pass
 router.post("/change-password",
     body("oldPassword").isString().notEmpty().withMessage("Old password is required"),
     body("newPassword").isString().notEmpty().withMessage("New password is required"),
@@ -194,6 +198,26 @@ router.post("/change-password",
     }
 )
 
+// generate token
+router.post("/generate-token", async (req, res) => {
+    const user = req.session.user;
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+    // Generate a token for the user
+    const token = await crypto.randomBytes(16).toString("hex");
+    await prisma.user.update({
+        where: {
+            id: user.id,
+        },
+        data: {
+            apiToken: token,
+        },
+    });
+
+    res.status(200).json({ apiToken: token });
+});
+
 // logout
 router.delete("/logout", async (req, res) => {
     req.session.destroy((err) => {
@@ -202,6 +226,37 @@ router.delete("/logout", async (req, res) => {
         }
         res.status(200).json({ message: "Logout successful" });
     });
+});
+
+// delete
+router.delete("/me", async (req, res) => {
+    const user = req.session.user;
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    // default 'admin' user cannot be deleted
+    if (user.username === "admin") {
+        return res.status(401).json({ message: "Default admin user cannot be deleted" });
+    }
+
+    const result = await prisma.user.delete({
+        where: {
+            id: user.id,
+        },
+    });
+
+    if (!result) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    try {
+        await req.session.destroy();
+    } catch (e) {   
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+
+    res.status(200).json({ message: "Account deleted successfully" });
 });
 
 export default router;
