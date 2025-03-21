b3199621da12ffbe65c7617a02f7e58222d3f4c0
Isira Adithya <rp.isira@gmail.com>
2025-03-21 20:26:46 +0530

    Added settings to schema with the startup script

--- server/backend/helpers/startup.js
+++ server/backend/helpers/startup.js
@@ -3,6 +3,44 @@ import crypto from "crypto";
 const prisma = new PrismaClient();
 import * as argon2 from "argon2";
 
+const SETTINGS = {
+    // notifications
+    notifications_enabled: true,
+
+    // smtp
+    smtp_host: "smtp.example.com",
+    smtp_port: 587,
+    smtp_user: "",
+    smtp_pass: "",
+    smtp_from: "",
+
+    // discord,slack,telegram
+    discord_webhook: "",
+    slack_webhook: "",
+    telegram_bot_token: "",
+    telegram_chat_id: "",
+
+    // ip identification
+    ipHeader: "X-Forwarded-For",
+}
+
+async function ensureSettings() {
+    // Create settings keys if not set
+    for (const key in SETTINGS) {
+        const setting = await prisma.settings.findUnique({
+            where: { key: key }
+        });
+        if (!setting) {
+            await prisma.settings.create({
+                data: {
+                    key: key,
+                    value: SETTINGS[key],
+                },
+            });
+        }
+    }
+}
+
 async function configureAdmin(){
     // Check if there is atleast one admin account present in the database
     const adminCount = await prisma.user.count({
@@ -32,4 +70,10 @@ async function configureAdmin(){
     }
 }
 
-export default configureAdmin;
+async function checkStartup(){
+    await ensureSettings();
+    await configureAdmin();
+    console.log("[LOG] Startup checks completed.");
+}
+
+export default checkStartup;
