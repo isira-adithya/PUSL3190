25291cb5b12d619a1526497e01d8a4e625d1fbfc
Isira Adithya <rp.isira@gmail.com>
2025-03-22 09:14:47 +0530

    Implemented Settings UI

---
+++ server/frontend/src/views/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-12 gap-8">
+    <div class="card col-span-12">
+      <Toast />
+      <ConfirmDialog group="positioned"></ConfirmDialog>
+      <NotificationsWidget />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import NotificationsWidget from '@/components/settings/NotificationsWidget.vue';
+import { onMounted } from 'vue';
+
+// Load settings on component mount
+onMounted(() => {
+});
+</script>
