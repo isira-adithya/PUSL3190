734cdf67465df2f855462f17c69145b19780b1a8
Isira Adithya <rp.isira@gmail.com>
2025-04-12 19:36:24 +0530

    admin password related changes

--- server/backend/helpers/startup.js
+++ server/backend/helpers/startup.js
@@ -1,4 +1,5 @@
 import { PrismaClient, UserRole } from "@prisma/client";
+import { readFileSync } from "fs";
 import crypto from "crypto";
 const prisma = new PrismaClient();
 import * as argon2 from "argon2";
@@ -61,9 +62,25 @@ async function configureAdmin(){
     if (adminCount > 0) {
         return;
     } else {
+
+        var password = null;
+        // if process.env.ADMIN_PASS_FILE is set, load it from process.env.ADMIN_PASS_FILE
+        if (!process.env.ADMIN_PASS_FILE) {
+            console.log(`Admin password file not set. Generating a new random password.`);
+        }
+        else {
+            console.log(`Loading admin password from ${process.env.ADMIN_PASS_FILE}`);
+            password = readFileSync(process.env.ADMIN_PASS_FILE, "utf8").trim();
+        }
+
+
         // Create a new admin account
         const username = "admin";
-        const password = crypto.randomBytes(16).toString("hex");
+        if (!password) {
+            password = crypto.randomBytes(16).toString("hex");
+            console.log(`[LOG] Admin account password: ${password}`);
+        }
+        
         // Hash the password using argon2
         const hashedPassword = await argon2.hash(password, {
             timeCost: 5,
@@ -76,7 +93,6 @@ async function configureAdmin(){
                 role: UserRole.ADMIN,
             },
         });
-        console.log(`[LOG] Admin account created with username: ${username} and password: ${password}`);
     }
 }
 
