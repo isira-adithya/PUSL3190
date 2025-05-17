1e0ff2cab636ff3eabe13657134cb58ecf7ec41e
Isira Adithya <rp.isira@gmail.com>
2025-05-17 22:47:43 +0530

    Refactor send notifications function to improve error handling and code readability

--- server/backend/helpers/send-notifications.js
+++ server/backend/helpers/send-notifications.js
@@ -1,9 +1,9 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import nodemailer from "nodemailer";
 import { prepareDiscordMessage, prepareSlackMessage, prepareTelegramMessage, prepareEmailHTML } from "./prepare-notifications.js";
 
-async function send(xssAlertId){
+async function send(xssAlertId) {
     // Check what channels are enabled for this alert
     const settings = await prisma.settings.findMany();
 
@@ -27,51 +27,57 @@ async function send(xssAlertId){
             document: true,
         }
     });
-    
-    // Send to Discord if enabled
-    if (discordEnabled) {
-        const discordMessage = prepareDiscordMessage(xssAlert);
-        const discordWebhookUrl = settings.find(setting => setting.key === 'discord_webhook').value;
-        const response = await fetch(discordWebhookUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(discordMessage)
-        });
-        if (response.status != 204){
-            console.log(`Discord response: ${response.status} ${response.statusText} - ${await response.text()}`);
+
+    try {
+        // Send to Discord if enabled
+        if (discordEnabled) {
+            const discordMessage = prepareDiscordMessage(xssAlert);
+            const discordWebhookUrl = settings.find(setting => setting.key === 'discord_webhook').value;
+            const response = await fetch(discordWebhookUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(discordMessage)
+            });
+            if (response.status != 204) {
+                console.log(`Discord response: ${response.status} ${response.statusText} - ${await response.text()}`);
+            }
         }
+
+        // Send to Slack if enabled
+        if (slackEnabled) {
+            const slackMessage = prepareSlackMessage(xssAlert);
+            const slackWebhookUrl = settings.find(setting => setting.key === 'slack_webhook').value;
+            const response = await fetch(slackWebhookUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(slackMessage)
+            });
+            if (response.status != 200) {
+                console.log(`Slack response: ${response.status} ${response.statusText} - ${await response.text()}`);
+            }
+        }
+
+        // Send to telegram if enabled
+        if (telegramEnabled) {
+            const telegramMessage = prepareTelegramMessage(xssAlert);
+            const telegramBotToken = settings.find(setting => setting.key === 'telegram_bot_token').value;
+            const telegramChatId = settings.find(setting => setting.key === 'telegram_chat_id').value;
+            const telegramUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+            const response = await fetch(telegramUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    chat_id: telegramChatId,
+                    text: telegramMessage,
+                    parse_mode: 'Markdown'
+                })
+            });
+        }
+    } catch (error) {
+        console.error(`Error sending notifications: ${error.message}`);
+        return;
     }
 
-    // Send to Slack if enabled
-    if (slackEnabled) {
-        const slackMessage = prepareSlackMessage(xssAlert);
-        const slackWebhookUrl = settings.find(setting => setting.key === 'slack_webhook').value;
-        const response = await fetch(slackWebhookUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(slackMessage)
-        });
-        if (response.status != 200){
-            console.log(`Slack response: ${response.status} ${response.statusText} - ${await response.text()}`);
-        }
-    }
-
-    // Send to telegram if enabled
-    if (telegramEnabled) {
-        const telegramMessage = prepareTelegramMessage(xssAlert);
-        const telegramBotToken = settings.find(setting => setting.key === 'telegram_bot_token').value;
-        const telegramChatId = settings.find(setting => setting.key === 'telegram_chat_id').value;
-        const telegramUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
-        const response = await fetch(telegramUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                chat_id: telegramChatId,
-                text: telegramMessage,
-                parse_mode: 'Markdown'
-            })
-        });
-    }
 
     // Send an Email if enabled
     if (emailsEnabled) {
@@ -82,7 +88,7 @@ async function send(xssAlertId){
         const smtpPass = settings.find(setting => setting.key === 'smtp_pass').value;
         const smtpHost = settings.find(setting => setting.key === 'smtp_host').value;
         const smtpPort = settings.find(setting => setting.key === 'smtp_port').value;
-        
+
         const transporter = nodemailer.createTransport({
             host: smtpHost,
             port: smtpPort,
@@ -110,18 +116,23 @@ async function send(xssAlertId){
         const emails = usersWithEmailsEnabled.map(user => user.email).join(',');
         console.log(`Sending email to: ${emails}`);
 
-        transporter.sendMail({
-            from: emailFrom,
-            to: emails,
-            subject: emailSubject,
-            html: emailMessage,
-        }, (error, info) => {
-            if (error) {
-                console.log(`Error sending email: ${error}`);
-            } else {
-                console.log(`Email sent: ${info.response}`);
-            }
-        });
+        try {
+            transporter.sendMail({
+                from: emailFrom,
+                to: emails,
+                subject: emailSubject,
+                html: emailMessage,
+            }, (error, info) => {
+                if (error) {
+                    console.log(`Error sending email: ${error}`);
+                } else {
+                    console.log(`Email sent: ${info.response}`);
+                }
+            });
+        } catch (error) {
+            console.error(`Error sending email: ${error}`);
+        }
+
     }
 }
 
